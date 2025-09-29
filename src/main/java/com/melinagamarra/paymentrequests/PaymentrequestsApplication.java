afe25612f80987c35d2c41f09f3a77a881102fc6
Melinagamarra-2023 <gamarramelina.2002@gmail.com>
2025-09-28 22:05:46 -0300

    Primer commit

---
+++ src/main/java/com/melinagamarra/paymentrequests/PaymentrequestsApplication.java
@@ -0,0 +1,13 @@
+package com.melinagamarra.paymentrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentrequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentrequestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melinagamarra/paymentrequests/PaymentrequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melinagamarra.paymentrequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentrequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
